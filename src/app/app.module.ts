6befa58b0447fc823300db80ff71768b9d5d805c
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-02-19 09:49:00 -0300

    Criação de componente home, loja e sobre nós

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LojaComponent } from './loja/loja.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LojaComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
