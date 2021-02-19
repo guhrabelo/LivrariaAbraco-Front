d207e30cb84836a2211d441a7314cb2c620d8223
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-02-19 20:39:25 -0300

    Teste

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,10 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
 }
+
+
+
+
