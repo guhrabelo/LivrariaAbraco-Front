b16e3ad3beff131e6a91c412c252767d84b5b6f2
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-02-24 08:43:25 -0300

    Criação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LojaComponent } from './loja/loja.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LojaComponent,
-    SobreNosComponent
+    SobreNosComponent,
+    CadastroComponent,
+    LoginComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
