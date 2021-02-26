b455ae967df8710c7327ed75b7aebe40885bcf09
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-02-26 08:36:55 -0300

    Criação de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  {path: 'home', component: HomeComponent},
+  {path: 'sobre-nos', component: SobreNosComponent},
+  {path: 'cadastro', component: CadastroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
